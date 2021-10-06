3b15e1e493e882e82f5493090203d62b66a147b7
Jared Frazier <cscidev001@gmail.com>
2021-10-06 15:07:19 -0500

    init ascent algorithm

--- greedy.py
+++ greedy.py
@@ -9,6 +9,7 @@ Description: Module for performing gradient ascent.
 
 import argparse
 import numpy as np
+import SumofGaussians
 
 
 def random_location(seed, d_dimensions, n_gaussians):
@@ -24,18 +25,48 @@ def random_location(seed, d_dimensions, n_gaussians):
 
 
 def gradient_ascent(
+        loc,
         iter_threshold=100000, delta_threshold=1e-8, step_size=0.01,
         print_last_only=True):
     """Performs gradient ascent.
 
-    :param iter_threshold: <lcass 'int'>
-    :param delta_threshold: <lcass 'float'>
+    :param loc: <class 'numpy.ndarray'> Location of point in 
+        d-dimensional space.
+    :param iter_threshold: <lcass 'int'> Max number of iterations
+        before termination of loop.
+    :param delta_threshold: <lcass 'float'> Threshold for change to
+        current location.
     :param step_size: <class 'float'>
     :param print_last_only: <class 'bool'>
 
     :return: None
     """
-    pass
+
+    ascend = True
+    iteration = 0
+    while ascend and iteration < iter_threshold:
+
+        # Calculate the change based on step size and derivative
+        # of the vector
+        delta = step_size * derivative(loc)
+
+        # Update location
+        loc = delta + loc
+
+        # Log location
+        if not print_last_only:
+            print(loc, delta)
+
+        # Check to see if absolute change is <= delta threshold
+        if abs(delta) <= delta_threshold:
+            ascend = False
+
+        # Update loop counter
+        iteration += 1
+
+    # Log last update location
+    if print_last_only:
+        print(loc, delta)
 
 
 def main():
