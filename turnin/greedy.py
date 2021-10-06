dc05a348376852cb8bf1eb66ca886c3856ad326b
Jared Frazier <cscidev001@gmail.com>
2021-10-06 14:51:36 -0500

    Init greedy

---
+++ turnin/greedy.py
@@ -0,0 +1,46 @@
+""" 
+Author: Jared Frazier
+Project: OLA 2
+File: greedy.py
+Class: CSCI 4350
+Instructor: Dr. Joshua Phillips
+Description: Module for performing gradient ascent.
+"""
+
+import argparse
+import numpy as np
+
+
+def random_location(seed, d_dimensions, n_gaussians):
+    """Generate a random location in a D-dimensional cube.
+
+    :param seed: <class 'int'>
+    :param d_dimensions: <class 'int'>
+    :param n_gaussians: <clas 'int'>
+
+    :return: <class 'numpy.ndarray'>
+    """
+    pass
+
+
+def gradient_ascent(
+        iter_threshold=100000, delta_threshold=1e-8, step_size=0.01,
+        print_last_only=True):
+    """Performs gradient ascent.
+
+    :param iter_threshold: <lcass 'int'>
+    :param delta_threshold: <lcass 'float'>
+    :param step_size: <class 'float'>
+    :param print_last_only: <class 'bool'>
+
+    :return: None
+    """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
